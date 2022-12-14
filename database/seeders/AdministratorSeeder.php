1646f776067ffd73fb3ba90905107ed70bccf279
arvandha121 <nauvan121@gmail.com>
2022-12-14 14:52:21 +0700

    first

---
+++ database/seeders/AdministratorSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $administrator = new \App\Models\User;
+        $administrator->username = "administrator";
+        $administrator->name = "Site Administrator";
+        $administrator->email = "admin@gmail.com";
+        $administrator->roles = json_encode(["ADMIN"]);
+        $administrator->password = Hash::make("password");
+        $administrator->avatar = "saat-ini-tidak-ada-file.png";
+        $administrator->address = "Sarmili, Bintaro, Tangerang Selatan";
+
+        $administrator->save();
+
+        $this->command->info("User Admin berhasil diinsert");
+    }
+}
